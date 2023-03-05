7fa8b9be4ecae243b995c8adae5164e880bb6607
Patryk <patrykprause@gmail.com>
2023-03-05 15:38:21 +0100

    Changes to be committed: 	modified:   gumroad_clone/settings.py

--- gumroad_clone/settings.py
+++ gumroad_clone/settings.py
@@ -101,21 +101,7 @@ WSGI_APPLICATION = "gumroad_clone.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': env('DATABASE_NAME'),
-#         'USER': env('DATABASE_USERNAME'),
-#         'PASSWORD': env('DATABASE_PASSWORD'),
-#         'HOST': env('MYSQL_HOST'),
-#     }
-# }
+
 DATABASES = {
     'default': dj_database_url.config(
         default=env('MYSQL_URL'),
